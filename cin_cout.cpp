d392cef28a782cb0727073947a90baad0b553be2
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-07 15:48:59 +0800

    Add two input

--- cin_cout.cpp
+++ cin_cout.cpp
@@ -2,11 +2,13 @@
 
 int main()
 {
-    std::cout << "Enter a number: "; // ask user for a number
+    std::cout << "Enter two numbers separated by a space: "; // ask user for two numbers
 
     int x{};            // define variable x to hold user input (zero-initialize by putting curly bracket {})
-    std::cin >> x;      // get number from user (keyboard) and store it in variable x
+    int y{};            // define variable y to hold user input (zero-initialize by putting curly bracket {})
+    
+    std::cin >> x >> y;      // get number from user (keyboard) and store it in variable x
 
-    std::cout << "You entered " << x << '\n'; // prefer use of '\n' compared to std::endl as the former doesn't request flush
+    std::cout << "You entered " << x << " and " << y << '\n'; // prefer use of '\n' compared to std::endl as the former doesn't request flush
     return 0;
 }
