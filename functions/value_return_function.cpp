c52a94cbadbd938bb0d4af24826943906a618508
abdulkhaliqmokhtar <abdulkhaliqmokhtar@gmail.com>
2022-12-13 20:39:50 +0800

    Test value return function

---
+++ functions/value_return_function.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+/*
+    getValueFromUser() function to return the value user entered back to main() so data can be make use in main()
+    void return function:   no value will be returned to the caller
+    int return function:    value type of int will be returned to the caller
+
+    Note: this doesn't determine what specific value is returned - only determines what type of value will be returned
+*/ 
+int getValueFromUser() // use int to return a value instead of void
+{
+    std::cout << "Enter an integer: ";
+    int userValue{};
+    std::cin >> userValue;
+
+    return userValue; // return the value user entered back to the caller (return by value)
+}
+
+int main()
+{
+    int num {getValueFromUser()}; // initialize num with the return value of getValueFromUser()
+
+    std::cout << num << " doubled is: " << num * 2 << '\n';
+
+    return 0; // status code or exit code (sometimes) or return code (rare)
+}
